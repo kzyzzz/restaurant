1786ab9424d3640a36be8083f864bb9ddb5586ad
VladimirB <kzyzzz@gmail.com>
2021-04-28 02:54:41 +0300

    contact section added, css to mobile-first

--- src/contact.js
+++ src/contact.js
@@ -3,17 +3,36 @@ const renderContact = () => {
 
     main.textContent = "";
 
-    const header = document.createElement('h1');
-    const text = document.createElement('p');
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
 
-    header.textContent = "Contact";
-    text.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
     
-    main.appendChild(header);
-    main.appendChild(text);
+    const leftText = document.createElement('div'); 
+
+    const map = document.createElement('img');
+    map.src = '../dist/img/map.png';
+    map.id = 'map';
+    leftText.appendChild(map);
+
+
+    // leftText.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    //                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    //                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    //                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+    const rightText = document.createElement('div');
+    
+    rightText.innerHTML = `<h1>Contact Us</h1>
+                           <p><i class="fa fa-map-marker" aria-hidden="true"></i> Friday Harbor Washington 98250 United States</p>
+                           <p><i class="fa fa-envelope-o" aria-hidden="true"></i> wine@loremipsum.com</p>`;
+
+    rightText.style.paddingRight = '1rem';
+    
+    contact.appendChild(rightText);
+    contact.appendChild(leftText);
+    
+    
+    main.appendChild(contact);
 };
 
 export default renderContact;
